034206d27fc215e5e32d00dfba101fe0e9c3b65e
Ali Amina <amina.thoueny@gmail.com>
2025-03-03 13:46:20 +0100

    Correction Git shallow.lock et GIT_DEPTH

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -6,11 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class SecurityControllerTest extends WebTestCase
 {
-    public function testIndex(): void
+    public function testLogin(): void
     {
         $client = static::createClient();
-        $client->request('GET', '/security');
+
+        $client->request('POST', '/api/login_check', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ], json_encode([
+            'username' => 'jean.dupont@example.com', 
+            'password' => 'password123'       
+        ]));
 
         self::assertResponseIsSuccessful();
+        self::assertJson($client->getResponse()->getContent());
     }
 }
